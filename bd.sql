45f802d376c2ad14d44fb4bb1cfb924451048988
CoderGustavo <gustavoornaghiantunes@gmail.com>
2021-08-12 09:36:50 -0300

    bd atualizado

--- bd.sql
+++ bd.sql
@@ -1,21 +1,19 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.4.16-MariaDB - mariadb.org binary distribution
+-- Server version:               5.6.12-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.1.0.6116
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2021-08-12 09:36:31
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
 -- Dumping database structure for lanchonete2
 CREATE DATABASE IF NOT EXISTS `lanchonete2` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `lanchonete2`;
+
 
 -- Dumping structure for table lanchonete2.admin
 CREATE TABLE IF NOT EXISTS `admin` (
@@ -26,11 +24,29 @@ CREATE TABLE IF NOT EXISTS `admin` (
   KEY `FK_idUsuario` (`id_usuario`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table lanchonete2.admin: 0 rows
+-- Dumping data for table lanchonete2.admin: 1 rows
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` (`id`, `id_usuario`, `niveis_acesso`) VALUES
 	(1, 2, '1');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+
+
+-- Dumping structure for table lanchonete2.avaliacao
+CREATE TABLE IF NOT EXISTS `avaliacao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `comentario` varchar(50) NOT NULL DEFAULT '',
+  `status` varchar(50) NOT NULL DEFAULT '',
+  `datahora` varchar(50) NOT NULL DEFAULT '',
+  `estrela` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK1_idUsuario` (`id_usuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table lanchonete2.avaliacao: 0 rows
+/*!40000 ALTER TABLE `avaliacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avaliacao` ENABLE KEYS */;
+
 
 -- Dumping structure for table lanchonete2.cardapio
 CREATE TABLE IF NOT EXISTS `cardapio` (
@@ -51,6 +67,7 @@ INSERT INTO `cardapio` (`id`, `id_categoria`, `nome`, `preco`, `descricao`, `ima
 	(2, 1, 'x-salada\r\n', 30, 'xtudão', NULL);
 /*!40000 ALTER TABLE `cardapio` ENABLE KEYS */;
 
+
 -- Dumping structure for table lanchonete2.categoria
 CREATE TABLE IF NOT EXISTS `categoria` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,23 +81,6 @@ INSERT INTO `categoria` (`id`, `nome`) VALUES
 	(1, 'lanche');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 
--- Dumping structure for table lanchonete2.avaliacao
-CREATE TABLE IF NOT EXISTS `avaliacao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_usuario` int(11) NOT NULL,
-  `avaliacao` varchar(50) NOT NULL DEFAULT '',
-  `status` varchar(50) NOT NULL DEFAULT '',
-  `data` varchar(50) NOT NULL DEFAULT '',
-  `estrela` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK1_idUsuario` (`id_usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
--- Dumping data for table lanchonete2.avaliacao: 1 rows
-/*!40000 ALTER TABLE `avaliacao` DISABLE KEYS */;
-INSERT INTO `avaliacao` (`id`, `id_usuario`, `avaliacao`, `status`, `data`, `estrela`) VALUES
-	(1, 2, 'muito legal', 'Pendente', '30/07/2021', 5);
-/*!40000 ALTER TABLE `avaliacao` ENABLE KEYS */;
 
 -- Dumping structure for table lanchonete2.endereco
 CREATE TABLE IF NOT EXISTS `endereco` (
@@ -100,6 +100,7 @@ INSERT INTO `endereco` (`id`, `id_Usuario`, `logradouro`, `numero`, `complemento
 	(1, 2, 'rua pedro consentini', 145, 'casa', 'verde vale');
 /*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
 
+
 -- Dumping structure for table lanchonete2.itens_pedido
 CREATE TABLE IF NOT EXISTS `itens_pedido` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -113,6 +114,7 @@ CREATE TABLE IF NOT EXISTS `itens_pedido` (
 -- Dumping data for table lanchonete2.itens_pedido: 0 rows
 /*!40000 ALTER TABLE `itens_pedido` DISABLE KEYS */;
 /*!40000 ALTER TABLE `itens_pedido` ENABLE KEYS */;
+
 
 -- Dumping structure for table lanchonete2.pedido
 CREATE TABLE IF NOT EXISTS `pedido` (
@@ -129,11 +131,12 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   KEY `id_endereco` (`id_endereco`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table lanchonete2.pedido: 0 rows
+-- Dumping data for table lanchonete2.pedido: 1 rows
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
 INSERT INTO `pedido` (`id`, `id_endereco`, `id_usuario`, `total`, `status`, `forma_pagamento`, `obs`, `entrega`) VALUES
 	(1, 1, 3, '50', 'Entregue', 'dinheiro', 'retira o tomate', 'sim');
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+
 
 -- Dumping structure for table lanchonete2.reserva
 CREATE TABLE IF NOT EXISTS `reserva` (
@@ -155,6 +158,7 @@ INSERT INTO `reserva` (`id`, `id_usuario`, `numero_pessoas`, `observacao`, `stat
 	(3, 2, 6, 'proximo a janela', 'Pendente', '15-04-2021 19:00');
 /*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
 
+
 -- Dumping structure for table lanchonete2.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -165,14 +169,11 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Dumping data for table lanchonete2.usuario: 1 rows
+-- Dumping data for table lanchonete2.usuario: 2 rows
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `telefone`) VALUES
 	(2, 'gustavo ornaghi antunes', 'gustavoornaghiantunes@gmail.com', '123', '(35) 98835-0841'),
 	(3, 'Gislaine Cristina Ornaghi', 'gislaine@gmail.com', '123', '35988350841');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
